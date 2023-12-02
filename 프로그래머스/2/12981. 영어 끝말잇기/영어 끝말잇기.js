4be6b204cfa1fb41a2f2cfe44bb6b52801c4f7f9
Dev.Jin <83911617+nara04040@users.noreply.github.com>
2023-12-02 19:21:57 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -1,19 +1,18 @@
-// n명이서 끝말잇기
-// 2명이서 hello(1), one(2), even(1), never(2), now(1) 라고 한다면
-// 가장 먼저 탈락하는 사람의 번호는 1번고 차례는 3번째이므로 return은 [1,3]이여야한다.
 function solution(n, words) {
-    
-    for(let i = 1; i < words.length; i++) {
-        let currentWord = words[i];
-        if(words.indexOf(currentWord) !== i) {
-            return [i % n + 1 , Math.floor(i/n) + 1];
-        }
-        const lastLetter = words[i - 1].slice(-1);
-        const firstLetter = currentWord.charAt(0);
-        if(lastLetter !== firstLetter){
-            return [i % n + 1, Math.floor(i/n) + 1];
+    const usedWords = new Set();  
+    let lastChar = words[0][0];  
+
+    for (let i = 0; i < words.length; i++) {
+        const word = words[i];
+
+        if (word[0] !== lastChar || usedWords.has(word)) {
+            return [(i % n) + 1, Math.floor(i / n) + 1];  
         }
 
-    }     
-    return [0,0];
+        usedWords.add(word); 
+        lastChar = word[word.length - 1];
+    }
+
+    return [0, 0];
 }
+
