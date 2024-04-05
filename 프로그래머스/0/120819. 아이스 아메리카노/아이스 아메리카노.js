cbc07a4d86bb4ec8560de2415ada3fd480e5b348
Dev.Jin <83911617+nara04040@users.noreply.github.com>
2024-04-05 14:48:45 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.js
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.js
@@ -1,8 +1,4 @@
 function solution(money) {
-    let maxCoffe = 0;
-    
-    maxCoffe = parseInt(money / 5500)
-    
-    return [maxCoffe, money % 5500]
-    
+    var answer = [];
+    return answer;
 }
