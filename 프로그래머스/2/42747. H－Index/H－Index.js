57a35873b2d996d9b162aac951f3bcf9a27bf177
Dev.Jin <83911617+nara04040@users.noreply.github.com>
2024-10-15 01:59:01 +0900

    [level 2] Title: H-Index, Time: 0.40 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.js
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -1,13 +1,14 @@
-// n : 발표한 논문.
-// citations : 논문의 "인용 횟수"를 담은 배열
-// 
-
 function solution(citations) {
-    let sortArr = citations.sort((a,b) => b-a);
-    for(let i = 0; i < citations.length; i++) {
-        if(i >= citations[i]) {
-            return i
+    citations.sort((a, b) => b - a);
+
+    let hIndex = 0;
+    for (let i = 0; i < citations.length; i++) {
+        if (citations[i] >= i + 1) {
+            hIndex = i + 1;
+        } else {
+            break;
         }
     }
-    return citations.length
+
+    return hIndex;
 }
