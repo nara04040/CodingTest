e7f7f720a20475128c1a0f3e97c114b9ef92c995
Dev.Jin <83911617+nara04040@users.noreply.github.com>
2024-11-30 15:54:03 +0900

    [level 1] Title: 약수의 합, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.js
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.js
@@ -3,7 +3,7 @@ let sum = 0;
     for(let i = 1; i <= Math.sqrt(n); i++) {
         if(n % i === 0) {
             sum += i;
-            if(i !== n/i) {  // 중복 방지 (예: 25의 경우 5가 중복)
+            if(i !== n/i) {  
                 sum += n/i;
             }
         }
