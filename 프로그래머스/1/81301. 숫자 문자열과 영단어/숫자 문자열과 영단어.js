e63f0ce794cf90db87596c5e2a267084b36122e8
Dev.Jin <83911617+nara04040@users.noreply.github.com>
2024-12-05 14:21:23 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.17 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,15 +1,22 @@
-// one4seveneight이면 1478로 변화해야한다.
-// 즉 숫자면 그대로 출력, string이면 문자열에 맞는 숫자로 변경하여 출력
-// 1. 숫자인지 문자열인지 판별
-// 2. 숫자라면 그대로 출력, 문자라면 단어에 속한 숫자로 변환
-
 function solution(s) {
-    let splitingChar = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
-    let answer = s
-    for(let i = 0; i < splitingChar.length; i++) {
-        let a = answer.split(splitingChar[i])
-        answer = a.join(i);
+    const numbers = {
+        'zero' : '0',
+        'one' : '1',
+        'two' : '2',
+        'three' : '3',
+        'four' : '4',
+        'five' : '5',
+        'six' : '6',
+        'seven' : '7',
+        'eight' : '8',
+        'nine' : '9'
+    };
+    
+    let answer = s;
+    
+    for(let [word, num] of Object.entries(numbers)) {
+        answer = answer.split(word).join(num);
     }
     
-    return Number(answer)
+    return parseInt(answer)
 }
