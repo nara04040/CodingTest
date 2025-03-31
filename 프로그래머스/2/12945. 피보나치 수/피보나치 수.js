eea8ec9b1bf835846f226eada0c4d1cc21a3ecd7
Dev.Jin <83911617+nara04040@users.noreply.github.com>
2025-03-31 17:49:08 +0900

    [level 2] Title: 피보나치 수, Time: 2.16 ms, Memory: 36.5 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
@@ -1,14 +1,15 @@
-// 피보나치수열을 정의하는 함수를 만들어라.
 function solution(n) {
-    let answer = 0
-    let f0 = 0;
-    let f1 = 1;
-    for(let i = 1; i < n; i++) {        
-        answer = (f0 + f1) % 1234567
-        f0 = f1;
-        f1 = answer
-    }
-    return answer
+  if(n <= 1) return n;
+
+  const MOD = 1234567;
+  let prev = 0;
+  let cur = 1;
+
+  for(let i = 2; i <= n; i++) {
+    const next = (prev + cur) % MOD;
+    prev = cur;
+    cur = next;
+  }
+
+  return cur
 }
-
-
