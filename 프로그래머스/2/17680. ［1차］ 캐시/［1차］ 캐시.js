63915df90bcd9767d932ae8a2125930c2b08700c
Dev.Jin <83911617+nara04040@users.noreply.github.com>
2025-04-11 23:06:22 +0900

    [level 2] Title: [1차] 캐시, Time: 24.50 ms, Memory: 41.8 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
@@ -1,21 +1,27 @@
 function solution(cacheSize, cities) {
+ // 캐시 크기가 0이면 모두 미스
     if (cacheSize === 0) return cities.length * 5;
     
+    // 캐시 배열 초기화
     const cache = [];
     let time = 0;
     
+    // 모든 도시를 소문자로 변환하여 처리
     cities.forEach(city => {
         city = city.toLowerCase();
         
+        // 캐시에 있는지 확인
         const index = cache.indexOf(city);
         
         if (index !== -1) {
+            // 캐시 히트: 해당 도시를 가장 최근으로 이동
             cache.splice(index, 1);
             cache.push(city);
             time += 1;
         } else {
+            // 캐시 미스: 새로운 도시 추가
             if (cache.length >= cacheSize) {
-                cache.shift(); 
+                cache.shift(); // 가장 오래된 도시 제거
             }
             cache.push(city);
             time += 5;
@@ -24,3 +30,4 @@ function solution(cacheSize, cities) {
     
     return time;
 }
+
